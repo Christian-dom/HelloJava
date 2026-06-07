a1d4e2670f18f0a3c2d1ead1e239a6566c38825f
christian-dom <chtistian.dominic00@gmail.com>
2026-06-07 14:13:49 +0700

    Feat(Variables): learning using variable and typing in java

---
+++ HelloWorld/src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        String fullName = "Christian Duong";
+        int age = 26;
+        double battery = 99.5;
+        boolean isDeveloper = true;
+        char myClass = 'S';
+
+        System.out.println(fullName);
+        System.out.println(age);
+        System.out.println(battery);
+        System.out.println(isDeveloper);
+        System.out.println(myClass);
+    }
+}
